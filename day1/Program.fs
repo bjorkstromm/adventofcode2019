42be7cd8eec73374a600810a6781df4c79996195
Martin Björkström <martin.bjorkstrom@gmail.com>
2019-12-02 00:16:28 +0200

    Day 1

---
+++ day1/Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// Formula for calculating fuel for module
+let fuelForModule mass =
+    (mass / 3) - 2
+
+// Recursive function to also take fuel for fuel into account
+let rec fuelForModule2 mass =
+    match fuelForModule mass with
+    | x when x < 1 -> 0
+    | x -> x + fuelForModule2 x
+
+// First version, doesn't take fuel for fuel into account
+let totalFuel modules =
+    modules
+    |> Array.sumBy fuelForModule
+
+// Second version, takes fuel for fuel into account
+let totalFuel2 modules =
+    modules
+    |> Array.sumBy fuelForModule2
+
+[<EntryPoint>] 
+let main argv =
+    argv
+    |> Array.map int
+    |> totalFuel2 
+    |> printfn "Total fuel needed is %i"
+    0 // return an integer exit code
